b3e7c9f4a1791cce0eb2830e96c31cbd94c9c10b
Baharul004 <34024851+Baharul004@users.noreply.github.com>
2018-03-06 18:32:17 +0100

    Update Connect.java

--- ImapServer/src/main/imapserver/Connect.java
+++ ImapServer/src/main/imapserver/Connect.java
@@ -1,10 +1,13 @@
 package main.imapserver;
 
 public class Connect {
+	/**
+	 * Client USER Name and Password
+	 */
 
 	private String USERNAME = "java";
 	private String PASSWORD = "jav";
-	private int PORT = 143;
+	private int PORT = 1143;
 	private String HOSTNAME = "127.0.0.1";
 
 	public String getUsername() {
